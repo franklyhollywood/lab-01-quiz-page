7d33511800ce5f68147e05738e337a17e9aafa33
Frank Hollywood <github@srvsync.com>
2021-08-16 13:35:16 -0700

    Initial content and button

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 // import functions and grab DOM elements
+const result = document.getElementById('resultsSpan');
+const button = document.getElementById('quizButton');
 
+console.log(result, button);
 // initialize state
 
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+// set event listeners
+// get user input
+// use user input to update state
+// update DOM to reflect the new state

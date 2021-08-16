9bb2146ba3d3917ad98cc2fef2b56c188500752a
Frank Hollywood <github@srvsync.com>
2021-08-16 15:43:10 -0700

    added test to function

--- test/example.test.js
+++ test/example.test.js
@@ -1,18 +1,18 @@
 // IMPORT MODULES under test here:
 // import { example } from '../example.js';
-
+import { answerCounts } from '../utils.js';
 const test = QUnit.test;
 
 test('time to test a function', (expect) => {
-    //Arrange
-    // Set up your arguments and expectations
-    const expected = true;
-    
-    //Act 
-    // Call the function you're testing and set the result to a const
-    const actual = true;
+	//Arrange
+	// Set up your arguments and expectations
+	const expected = true;
 
-    //Expect
-    // Make assertions about what is expected versus the actual result
-    expect.equal(actual, expected);
+	//Act
+	// Call the function you're testing and set the result to a const
+	const actual = answerCounts('yeeeees');
+
+	//Expect
+	// Make assertions about what is expected versus the actual result
+	expect.equal(actual, expected);
 });

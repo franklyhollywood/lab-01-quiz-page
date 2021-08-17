951abe3960fb76ace3a0b0d429653e1f3962cc28
Frank Hollywood <github@srvsync.com>
2021-08-16 17:31:17 -0700

    Added TDD test for No

---
+++ test/utils.test.js
@@ -0,0 +1,16 @@
+import { answerCounts } from '../utils.js';
+const test = QUnit.test;
+
+test('time to test a function', (expect) => {
+	const expected = true;
+	const actual = answerCounts('yeeeees');
+
+	expect.equal(actual, expected);
+});
+
+test('time to test a function', (expect) => {
+	const expected = true;
+	const actual = answerCounts('noooooo');
+
+	expect.equal(actual, expected);
+});
